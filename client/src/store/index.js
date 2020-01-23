25150addbd3f901644b7d9ba29af2e3533b5ec80
ninoloid <muhammadsatriaadiputra@gmail.com>
2020-01-23 21:04:38 +0700

    masih proses

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,6 +5,13 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    questions: [
+      { "pertanyaan 1": [{ "jawaban 1": true }, { "jawaban 2": false }, { "jawaban 3": false }, { "jawaban 4": false }] },
+      { "pertanyaan 2": [{ "jawaban 1": false }, { "jawaban 2": false }, { "jawaban 3": true }, { "jawaban 4": false }] },
+      { "pertanyaan 3": [{ "jawaban 1": false }, { "jawaban 2": true }, { "jawaban 3": false }, { "jawaban 4": false }] },
+      { "pertanyaan 4": [{ "jawaban 1": false }, { "jawaban 2": false }, { "jawaban 3": false }, { "jawaban 4": true }] },
+      { "pertanyaan 5": [{ "jawaban 1": false }, { "jawaban 2": true }, { "jawaban 3": false }, { "jawaban 4": false }] }
+    ]
   },
   mutations: {
   },
---
+++ client/src/views/GameRoom.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="game">
+    <div class="card w-75 text-center py-4 mx-auto"></div>
+    <div class="row">{{ question }}</div>
+    <div class="row">
+      <div class="col">{{ answer }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      questions: this.$store.state.questions,
+      question: '',
+      answer:[],
+      object: {},
+      result: {
+        corrects:0,
+        incorrects:0
+      }
+    }
+  },
+  created() {
+    console.log('cek datanya ==========>', this.questions)
+    this.getQuestion()
+  },
+  methods : {
+    getQuestion: function(answer) {
+      this.object = this.questions.shift()
+      this.question = Object.keys(this.object)[0]
+      this.answer = Object.values(this.object)[0]
+      console.log('pertanyaannya    :', this.question)
+      console.log('jawabannya       :', this.answer)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
