aa9ac586df124ee3aa666806d67bba1598f2da4b
GanangPrakoso <ganangmanggolo@gmail.com>
2020-01-23 20:34:05 +0700

    eslint zzzzzz

---
+++ client/src/components/Results.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+     <div class="row">
+        <div class="col">
+           <h1>hohohoho</h1>
+        </div>
+        <div class="col">
+           <h1>100</h1>
+        </div>
+     </div>
+  </div>
+</template>
+
+<script>
+export default {
+   name : `Results`
+}
+</script>
+
+<style>
+
+</style>
