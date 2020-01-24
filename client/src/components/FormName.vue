297d4553f81ea0e7a0398a55cde69b3936904b0c
ivan <ivantjendra@gmail.com>
2020-01-24 11:00:45 +0700

    create room and enter room

---
+++ client/src/components/FormName.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="col">
+    <form @submit.prevent="addName">
+      <div class="form-group">
+        <label for="playerName">Player Name</label>
+        <input type="text" class="form-control" id="playerName" placeholder="Gladiator" required
+          v-model="playerName"
+        >
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      playerName: ''
+    }
+  },
+  methods: {
+    addName () {
+      localStorage.setItem('playerName', this.playerName)
+      this.$router.push('/rooms')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
