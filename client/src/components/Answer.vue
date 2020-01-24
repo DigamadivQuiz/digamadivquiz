7215dd49a3d14f4478903ab9668d99550e29bd65
GanangPrakoso <ganangmanggolo@gmail.com>
2020-01-24 16:36:01 +0700

    add sound

--- client/src/components/Answer.vue
+++ client/src/components/Answer.vue
@@ -3,8 +3,8 @@
     <div class="row">
       <div class="col">
         <div v-for="(ans, i) in answer" :key="i">
-          <Button type="button" class="btn btn-secondary px-4 mx-2 my-2"  v-if="i%2 == 0" @click.prevent="isTrue(ans)">{{ Object.values(ans)[0] }}</Button>
-          <Button type="button" class="btn btn-dark px-4 mx-2 my-2"  v-if="i%2 !== 0" @click.prevent="isTrue(ans)">{{ Object.values(ans)[0] }}</Button>    
+          <Button type="button" class="btn btn-secondary px-4 mx-2 my-2"  v-if="i%2 == 0" @click.prevent="isTrue(ans)" v-on:click.prevent="playSound">{{ Object.values(ans)[0] }}</Button>
+          <Button type="button" class="btn btn-dark px-4 mx-2 my-2"  v-if="i%2 !== 0" @click.prevent="isTrue(ans)" v-on:click.prevent="playSound">{{ Object.values(ans)[0] }}</Button>    
         </div>
       </div>
     </div>
@@ -14,7 +14,9 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+
+    }
   },
   props: {
     answer: Array,
@@ -27,6 +29,11 @@ export default {
       console.log("checker", checker);
       if (this.score === this.currentScore && checker)
         this.$emit("update-score", 20);
+    },
+    playSound() {
+      // sound = this.sound
+      let audio = new Audio('http://soundbible.com/mp3/Air Plane Ding-SoundBible.com-496729130.mp3')
+      audio.play()
     }
   }
 };
